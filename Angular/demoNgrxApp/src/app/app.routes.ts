bcd0f071da66fc8e3662a9956d367450836bf6ab
trupti diwan <truptid969@gmail.com>
2024-10-05 01:21:12 +0530

    Added component ,routing, lazyloading

--- Angular/demoNgrxApp/src/app/app.routes.ts
+++ Angular/demoNgrxApp/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: 'home', component: HomeComponent },
+  { path: 'home', component: CounterComponent },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./contact/contact.module').then((m) => m.ContactModule),
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
